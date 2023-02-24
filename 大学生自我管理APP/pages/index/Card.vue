494c0163f4c18ae302db5d2c147be3a1f8daf73c
Fanrito <2902566341@qq.com>
2023-02-24 20:35:59 +0800

    feat: 完成单个课程表组件的前端页面，初始化课程表组件的后端

---
+++ 大学生自我管理APP/pages/index/Card.vue
@@ -0,0 +1,36 @@
+<template>
+  <uni-card is-full="true" padding="2px 0">
+    <text class="course">{{ data.course }}</text>
+    <br>
+    <text class="teacher">{{ data.teacher }}</text>
+    <br>
+    <text class="adress">{{ data.adress }}</text>
+    <br>
+    <text class="score">{{ data.score }}学分</text>
+  </uni-card>
+</template>
+
+<script setup>
+  const props = defineProps({
+    data: Object
+  })
+</script>
+
+<style lang="scss">
+  .course {
+    font-size: 10px;
+    font-weight: 700;
+  }
+
+  .teacher {
+    font-size: 8px;
+  }
+
+  .adress {
+    font-size: 8px;
+  }
+
+  .score {
+    font-size: 8px;
+  }
+</style>
---
+++ 大学生自我管理APP/pages/index/data/week.js
@@ -0,0 +1,21 @@
+export default [{
+    id: 1,
+    title: '周一'
+  },
+  {
+    id: 2,
+    title: '周二'
+  },
+  {
+    id: 3,
+    title: '周三'
+  },
+  {
+    id: 4,
+    title: '周四'
+  },
+  {
+    id: 5,
+    title: '周五'
+  }
+]
